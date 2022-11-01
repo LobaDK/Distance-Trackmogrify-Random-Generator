1d0ede970c2206a48255ea278c3e7a0118ba07f6
Nicklas <nicklashelsberg@gmail.com>
2022-11-01 14:08:04 +0100

    Main rework done

--- Distance Trackmogrify Random Generator/Distance_Trackmogrify_Random_Generator.py
+++ Distance Trackmogrify Random Generator/Distance_Trackmogrify_Random_Generator.py
@@ -1,5 +1,7 @@
-import random
 import os
+import random
+import string
+
 try:
     import pyperclip
 except:
@@ -7,110 +9,422 @@ except:
 
 os.system('mode 200,20')
 while(True):
-    Difficulty = ["Casual","Trivial","Safe","Easy","Normal","Medium","Advanced","Hard","Expert","Nightmare","Doom","Challenging","Maniac","Hazardous","Extreme"]
-    Color = ["Red","Blue","Green","Yellow","Purple","Pink","Orange","Black","White","Grey","Tan"]
-    Advanced_Color = ["Iridescent","Pearl","Monochromatic","Analogous","Triad","Complementary","Compound","Shades","Perlin"]
-    Lighting = ["Iridescent","Desaturated","Saturated","Bright","Dim","Mute"]
-    Track_Lighting = ["Iridescent","Desaturated","Saturated","Bright","Dim","Mute"]
-    Global_Lighting = ["Sunrise","Morning","Day","Afternoon","Evening","Sunset","Night","Midnight","Light","Dark","Shadow","Moon"]
-    Environment = ["Fog","Mist","Smoke","Ooze","Slime","Radioactive","Radiation","Biohazard","Storm","Thunder","Lightning","Gas","Ocean","Water","Marine","Sea"]
-    Theme = ["space","Nightmare","Water","Dark","Fire","Not","Tron","Slime","Zebra","Rainbow","America","Stone","Transparent","Metallic","Metal","Ice","Neon","Copper","Silver","Gold","Platinum","Brass","Iron","Steel","Aluminum","Bronze","Titanium"]
-    Elevation = ["Uphill","Climb","Ascent","Rise","Elevate","Downhill","Descent","Fall","Down","Lower"]
-    Style = ["Twisty","Crazy","Bumpy","Road","Straight","Chill","Boring","Angry","Flat","Dull","Hilly","Zigzag","Loopy","UpsideDown","Snakey","Turn","Corkscrew","Helix","Inverted","Ceiling","Bouncy","Winding","Rollercoaster","Screwy"]
-    Length = ["Fleeting","Momentary","Short","Quick","Long","Marathon"]
-    Hazard = ["Pop-up","Barrier","Jump","Stop","Breakable","Egg","Roller","Laser","Saw","Shard","Dropper","Pumpkin"]
-    Scenery = ["Empire","Virus","Infected","Ancient","Old","Broken","Simple","Toon","Minecraft","Halloween","Spooky","Graveyard","Round","Blocky","Angled","Spiky","Cube","Plane","Sphere","Cylinder","Hexagon","Triangle","Ring","Capsule","Transparent","Glow","Reflective","Flashing","Party","Disco","Dance","Dull","Speckledlights","Striped","Untextured","Rocklike","Factory","Plant","Industrial","City","Alien","Nature","Forest","Moving","Breakable","Alliancelogo","Logo","Sponsored","Cluster","Sign","Drone"]
-    Size = ["Big","Super","Monster","Small","Tiny","Mini","Shrunk","Micro","Giant","Colossus"]
-    Placement = ["Vast","Tight","Space","Above","Under","Below","Uniform"]
-    Intensity = ["Barely","Slightly","Kinda","So","Very","Double","Super","Triple","Mega","Ultra","Insanely"]
 
+    characters = string.ascii_letters + string.digits
 
-    RNGOutput = []
-    Output = ""
-    Choice = ""
-    Color_Choice = ""
-    Advanced_Color_Choice = ""
-    Lighting_Choice = ""
-    Track_Lighting_Choice = ""
-    Global_Lighting_Choice = ""
-    Environment_Choice = ""
-    Theme_Choice = ""
-    Elevation_Choice = ""
-    Style_Choice = ""
-    Length_Choice = ""
-    Hazard_Choice = ""
-    Scenery_Choice = ""
-    Size_Choice = ""
-    Placement_Choice = ""
-    Intensity_Choice = ""
+    Misc_color_mod = ['Rainbow',
+                'Neon',
+                'Tron',
+                'America',
+                'Transparent',
+                'Zebra',
+                'Stone',
+                'Flame',
+                'Fire',
+                'Ice',
+                'Metallic',
+                'Metal',
+                'Copper',
+                'Gold',
+                'Silver',
+                'Platinum',
+                'Brass',
+                'Iron',
+                'Steel',
+                'Aluminum',
+                'Bronze',
+                'Titanium'
+                ]
 
-    #Randomly add a difficulty
-    Output = random.choice(Difficulty)
-    #Randomly add between 0 and 2 colors
-    RNGOutput += random.sample(Color, k=random.randint(0,2))
-    if RNGOutput:
-        Output += " " + " ".join(RNGOutput)
-    RNGOutput = []
-    #Randomly add between 0 and 2 Advanced colors
-    RNGOutput += random.sample(Advanced_Color, k=random.randint(0,2))
-    if RNGOutput:
-        Output += " " + " ".join(RNGOutput)
-    RNGOutput = []
-    #Randomly add between 0 and 2 Lighting
-    RNGOutput += random.sample(Lighting, k=random.randint(0,2))
-    if RNGOutput:
-        Output += " " + " ".join(RNGOutput)
-    RNGOutput = []
-    #Randomly add between 0 and 2 Track_Lighting
-    RNGOutput += random.sample(Track_Lighting, k=random.randint(0,2))
-    if RNGOutput:
-        Output += " " + " ".join(RNGOutput)
-    RNGOutput = []
-    #Randomly add between 0 and 3 Global_Lighting
-    RNGOutput += random.sample(Global_Lighting, k=random.randint(0,3))
-    if RNGOutput:
-        Output += " " + " ".join(RNGOutput)
-    RNGOutput = []
-    #Randomly add between 0 and 3 Environment
-    RNGOutput += random.sample(Environment, k=random.randint(0,3))
-    if RNGOutput:
-        Output += " " + " ".join(RNGOutput)
-    RNGOutput = []
-    #Randomly add between 0 and 2 Theme
-    RNGOutput += random.sample(Theme, k=random.randint(0,2))
-    if RNGOutput:    
-        Output += " " + " ".join(RNGOutput)
-    RNGOutput = []
-    #Randomly add between 0 and 3 Elevation
-    RNGOutput += random.sample(Elevation, k=random.randint(0,3))
-    if RNGOutput:
-        Output += " " + " ".join(RNGOutput)
-    RNGOutput = []
-    #Randomly add between 0 and 3 Style
-    RNGOutput += random.sample(Style, k=random.randint(0,3))
-    if RNGOutput:
-        Output += " " + " ".join(RNGOutput)
-    RNGOutput = []
-    #Randomly add Length
-    #RNGOutput += random.choices(Length,cum_weights=(5,15,35,60,90,115),k=1)
-    Output += " " + " ".join(random.choices(Length,cum_weights=(5,15,35,60,90,115),k=1))
-    RNGOutput = []
-    #Randomly add between 1 and 12 Hazard
-    #RNGOutput += random.sample(Hazard, k=random.randint(1,12))
-    Output += " " + " ".join(random.sample(Hazard, k=random.randint(1,12)))
-    RNGOutput = []
-    #Randomly add between 0 and 3 Scenery
-    RNGOutput += random.sample(Scenery, k=random.randint(0,3))
-    if RNGOutput:
-        Output += " " + " ".join(RNGOutput)
-    RNGOutput = []
-    #Randomly add Size
-    Output += " " + random.choice(Size)
-    #Randomly add Placement
-    Output += " " + random.choice(Placement)
-    #Randomly add Intensity
-    Output += " " + random.choice(Intensity)
-    
+    Color_mod = ['indianRed',
+                'lightCoral',
+                'salmon',
+                'darkSalmon',
+                'lightSalmon',
+                'crimson',
+                'red',
+                'fireBrick',
+                'darkRed',
+                'pink',
+                'lightPink',
+                'hotPink',
+                'deepPink',
+                'mediumVioletRed',
+                'paleVioletRed',
+                'coral',
+                'tomato',
+                'orangeRed',
+                'darkOrange',
+                'orange',
+                'yellow',
+                'lightYellow',
+                'lemonChiffon',
+                'lightGoldenrodYellow',
+                'papayaWhip',
+                'moccasin',
+                'peachPuff',
+                'paleGoldenrod',
+                'khaki',
+                'darkKhaki',
+                'lavender',
+                'thistle',
+                'plum',
+                'violet',
+                'orchid',
+                'fuchsia',
+                'magenta',
+                'mediumOrchid',
+                'mediumPurple',
+                'amethyst',
+                'blueViolet',
+                'darkViolet',
+                'darkOrchid',
+                'darkMagenta',
+                'purple',
+                'indigo',
+                'slateBlue',
+                'darkSlateBlue',
+                'mediumSlateBlue',
+                'greenYellow',
+                'chartreuse',
+                'lawnGreen',
+                'lime',
+                'limeGreen',
+                'paleGreen',
+                'lightGreen',
+                'mediumSpringGreen',
+                'springGreen',
+                'mediumSeaGreen',
+                'seaGreen',
+                'forestGreen',
+                'green',
+                'darkGreen',
+                'yellowGreen',
+                'oliveDrab',
+                'olive',
+                'darkOliveGreen',
+                'mediumAquamarine',
+                'darkSeaGreen',
+                'lightSeaGreen',
+                'darkCyan',
+                'teal',
+                'aqua',
+                'cyan',
+                'lightCyan',
+                'paleTurquoise',
+                'aquamarine',
+                'turquoise',
+                'mediumTurquoise',
+                'darkTurquoise',
+                'cadetBlue',
+                'steelBlue',
+                'lightSteelBlue',
+                'powderBlue',
+                'lightBlue',
+                'skyBlue',
+                'lightSkyBlue',
+                'deepSkyBlue',
+                'dodgerBlue',
+                'cornflowerBlue',
+                'royalBlue',
+                'blue',
+                'mediumBlue',
+                'darkBlue',
+                'navy',
+                'midnightBlue',
+                'cornsilk',
+                'blanchedAlmond',
+                'bisque',
+                'navajoWhite',
+                'wheat',
+                'burlyWood',
+                'tan',
+                'rosyBrown',
+                'sandyBrown',
+                'goldenrod',
+                'darkGoldenrod',
+                'peru',
+                'chocolate',
+                'saddleBrown',
+                'sienna',
+                'brown',
+                'maroon',
+                'white',
+                'snow',
+                'honeydew',
+                'mintCream',
+                'azure',
+                'aliceBlue',
+                'ghostWhite',
+                'whiteSmoke',
+                'seashell',
+                'beige',
+                'oldLace',
+                'floralWhite',
+                'ivory',
+                'antiqueWhite',
+                'linen',
+                'lavenderBlush',
+                'mistyRose',
+                'gainsboro',
+                'lightGrey',
+                'darkGray',
+                'gray',
+                'dimGray',
+                'lightSlateGray',
+                'slateGray',
+                'darkSlateGray',
+                'black'
+                ]
+
+    Track_lighting_mod = ['Iridescent',
+                    'Desaturated',
+                    'Saturated',
+                    'Bright',
+                    'Dim',
+                    'Mute'
+                    ]
+
+    Environment_mod = ['Fog',
+                    'Mist',
+                    'Smoke',
+                    'Ooze',
+                    'Slime',
+                    'Radioactive',
+                    'Radiation',
+                    'Biohazard',
+                    'Storm',
+                    'Thunder',
+                    'Lightning',
+                    'Gas',
+                    'Ocean',
+                    'Water',
+                    'Marine',
+                    'Sea'
+                    ]
+
+    Difficulty_mod = ['Casual',
+                    'Trivial',
+                    'Safe',
+                    'Easy',
+                    'Normal',
+                    'Medium',
+                    'Advanced',
+                    'Hard',
+                    'Expert',
+                    'Nightmare',
+                    'Doom',
+                    'Challenging',
+                    'Maniac',
+                    'Hazardous',
+                    'Extreme'
+                    ]
+
+    Elevation_mod = ['Uphill',
+                    'Climb',
+                    'Ascent',
+                    'Rise',
+                    'Elevate',
+                    'Downhill',
+                    'Descent',
+                    'Fall',
+                    'Down',
+                    'Lower'
+                    ]
+
+    Style_mod = ['Twisty',
+                'Crazy',
+                'Bumpy',
+                'Road',
+                'Straight',
+                'Straightaway',
+                'Chill',
+                'Boring',
+                'Angry',
+                'Flat',
+                'Dull',
+                'Hilly',
+                'Zigzag',
+                'Loopy',
+                'UpsideDown',
+                'Snaky',
+                'Turn',
+                'Corkscrew',
+                'Helix',
+                'Inverse',
+                'Inverted',
+                'Ceiling',
+                'Bouncy',
+                'Winding',
+                'Rollercoaster',
+                'Screwy',
+                ]
+
+    Scenery_type_mod = ['Empire',
+                        'Virus',
+                        'Infected',
+                        'Ancient',
+                        'Old',
+                        'Broken',
+                        'Simple',
+                        'Toon',
+                        'Minecraft',
+                        'Halloween',
+                        'Spooky',
+                        'Graveyard',
+                        'Round',
+                        'Blocky',
+                        'Angled',
+                        'Spiky',
+                        'Spike',
+                        'Cube',
+                        'Box',
+                        'Plane',
+                        'Sphere',
+                        'Ball',
+                        'Cylinder',
+                        'Tube',
+                        'Hexagon',
+                        'Hex',
+                        'Triangle',
+                        'Ring',
+                        'Donut',
+                        'Cone',
+                        'Capsule',
+                        'Pill',
+                        'Transparent',
+                        'Clear',
+                        'Glow',
+                        'Reflective',
+                        'Flashing',
+                        'Party',
+                        'Disco',
+                        'Dance',
+                        'Dull',
+                        'Speckledlights',
+                        'Striped',
+                        'Untextured',
+                        'Rocklike',
+                        'Factory',
+                        'Plant',
+                        'Industrial',
+                        'City',
+                        'Alien',
+                        'Nature',
+                        'Forest',
+                        'Moving',
+                        'Breakable',
+                        'Alliancelogo',
+                        'Alliance',
+                        'Logo',
+                        'Sponsored',
+                        'Cluster',
+                        'Sign',
+                        'Drone'
+                        ]
+
+    Scenery_size_mod = ['Big',
+                        'Super',
+                        'Monster',
+                        'Small',
+                        'Tiny',
+                        'Mini',
+                        'Shrunk',
+                        'Micro',
+                        'Giant',
+                        'Colossus'
+                        ]
+
+    Scenery_placement_mod = ['Vast',
+                            'Tight',
+                            'Space',
+                            'Above',
+                            'Under',
+                            'Below',
+                            'Uniform'
+                            ]
+
+    Intensity_mod = ['Barely',
+                    'Slightly',
+                    'Kinda',
+                    'So',
+                    'Very',
+                    'Double',
+                    'Super',
+                    'Triple',
+                    'Mega',
+                    'Ultra',
+                    'Insanely'
+                    ]
+
+    Advanced_color_mod = ['Iridescent',
+                        'Pearl',
+                        'Monochromatic',
+                        'Analogous',
+                        'Triad',
+                        'Complementary',
+                        'Compound',
+                        'Shades',
+                        'Perlin'
+                        ]
+
+    Global_lighting_mod = ['Sunrise',
+                        'Morning',
+                        'Day',
+                        'Afternoon',
+                        'Evening',
+                        'Sunset',
+                        'Night',
+                        'Midnight',
+                        'Light',
+                        'Dark',
+                        'Shadow',
+                        'Moon'
+                        ]
+
+    Lenght_mod = ['Fleeting',
+                'Momentary',
+                'Short',
+                'Quick',
+                'Long',
+                'Marathon'
+                ]
+
+    Hazard_mod = ['Pop-Up',
+                'Barrier',
+                'Jump',
+                'Stop',
+                'Breakable',
+                'Egg',
+                'Roller',
+                'Laser',
+                'Saw',
+                'Shard',
+                'Dropper',
+                'Pumpkin'
+                ]
+
+    Output = '{Misc_color} {Color} {Track_lighting} {Environment} {Difficulty} {Elevation} {Style} {Scenery_type} {Scenery_size} {Scenery_placement} {Intensity} {Advanced_color} {Global_lighting} {Lenght} {Hazard} {Salt}'.format(
+                            Misc_color=" ".join(random.sample(Misc_color_mod, k=random.randint(0,1))),
+                            Color=" ".join(random.sample(Color_mod, k=random.randint(0,4))),
+                            Track_lighting=" ".join(random.sample(Track_lighting_mod, k=random.randint(0,1))),
+                            Environment=" ".join(random.sample(Environment_mod, k=random.randint(0,3))),
+                            Difficulty=" ".join(random.choices(Difficulty_mod, k=1, weights=(5,10,15,20,50,70,90,100,100,100,100,100,100,100,100))),
+                            Elevation=" ".join(random.sample(Elevation_mod, k=random.randint(0,3))),
+                            Style=" ".join(random.sample(Style_mod, k=random.randint(0,3))),
+                            Scenery_type=" ".join(random.sample(Scenery_type_mod, k=random.randint(0,3))),
+                            Scenery_size=random.choice(Scenery_size_mod),
+                            Scenery_placement=random.choice(Scenery_placement_mod),
+                            Intensity=random.choice(Intensity_mod),
+                            Advanced_color=" ".join(random.sample(Advanced_color_mod, k=random.randint(0,2))),
+                            Global_lighting=" ".join(random.sample(Global_lighting_mod, k=random.randint(0,1))),
+                            Lenght=" ".join(random.choices(Lenght_mod, k=1, weights=(5, 10, 20, 40, 100, 80))),
+                            Hazard=" ".join(random.sample(Hazard_mod, k=random.randint(0,7))),
+                            Salt="".join(random.choice(characters) for _ in range(random.randint(5,40)))
+                            )
+
+    Output = " ".join(Output.split())
+
     try:
         pyperclip.copy(Output)
         print("Sequence has been copied to clipboard.\n\n",Output)
